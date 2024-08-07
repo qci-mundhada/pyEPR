aaedbd7da338922143284615304008daf0a13117
Shantanu Mundhada <mundhada@quantumcircuits.com>
2024-08-07 13:30:30 -0400

    SM: Commiting some fixes to pyEPR

--- pyEPR/_config_user.py
+++ pyEPR/_config_user.py
@@ -21,7 +21,7 @@ config = Dict( # pylint: disable=invalid-name
 
     # Folder to save result data to.
     # PLEASE CHANGE THIS
-    root_dir=r'C:\Ansoft\Shantanu_M\EBBQ_Data',
+    root_dir=r'D:\EBBQ_Data',
 
     # Loss properties of various materials and surfaces
     dissipation=Dict(
